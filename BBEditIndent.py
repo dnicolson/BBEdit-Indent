6baa54990a7379c586e575779fa3f9f51cc50f5d
David Nicolson <dnicolson@rchigl009Ameglocal.local>
2013-08-19 17:34:07 +1000

    Initial commit

---
+++ BBEditIndent.py
@@ -0,0 +1,60 @@
+import sublime, sublime_plugin
+
+class BbeditIndentCommand(sublime_plugin.TextCommand):
+    def run(self, edit, action):
+        view = self.view
+
+        settings = view.settings()
+        tab_size = int(settings.get('tab_size', 4))
+        use_spaces = settings.get('translate_tabs_to_spaces')
+
+        # iterate over selections
+        for sel in view.sel():
+            points = []
+
+            # iterate over lines
+            for line in view.lines(sel):
+
+                # get point at the start of the line
+                pt = line.begin()
+                char = view.substr(pt)
+
+                # loop through line whitespace
+                while char == ' ' or char == '\t':
+                    pt += 1
+                    char = view.substr(pt)
+
+                # ignore blank lines
+                if view.rowcol(pt+1)[0] != view.rowcol(line.begin())[0]:
+                    continue
+
+                points.append(pt)
+
+            pt_count = 0
+            for pt in points:
+                if action == 'indent':
+                    view.insert(edit, pt + pt_count, ' ')
+                    # check if last inserted characters can be converted to a tab
+                    if not use_spaces and view.substr(sublime.Region(pt + pt_count - (tab_size - 1),pt + pt_count+1)) == ' ' * tab_size:
+                        view.replace(edit,sublime.Region(pt + pt_count - (tab_size - 1),pt + pt_count + 1),'\t')
+                        pt_count -= tab_size - 1
+                    pt_count += 1
+                elif action == 'unindent':
+                    start = pt - pt_count - 1
+                    end = pt - pt_count
+
+                    # remove spaces if available
+                    if view.rowcol(start)[0] == view.rowcol(end)[0]:
+
+                        # replace tab with spaces
+                        if view.substr(start) == '\t':
+                            view.replace(edit,sublime.Region(start,end),' ' * (tab_size - 1))
+                            pt_count -= tab_size - 2
+                        else:
+                            view.erase(edit, sublime.Region(start,end))
+                            pt_count += 1
+
+                    # remove one space if available
+                    elif view.rowcol(start)[0] == view.rowcol(start + 1)[0]:
+                        view.erase(edit, sublime.Region(start,start + 1))
+                        pt_count += 1
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-BBEditIndent
+BBEdit Indent
 ============
+A simple plugin that enables indentation by a single character at a time when `cmd-shift-[` or `cmd-shift-]` is pressed. This overrides the the `prev_view` and `next_view` commands (`cmd-alt-left` and `cmd-alt-right` continue to be used for the `prev_view` and `next_view` commands).
+
+This matches the behaviour of [BBEdit](http://www.barebones.com/products/bbedit/) and [TextWrangler](http://www.barebones.com/products/textwrangler/).
